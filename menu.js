8918937c069d3f87b69670ce76325fb8be1a3855
Vincenzo jr <161210983+Vincenzo08jr@users.noreply.github.com>
2025-05-10 16:36:06 +0200

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,40 @@
+// JavaScript Document
+
+<!-- Begin
+
+// NOTE: If you use a ' add a slash before it like this \'
+// USE lowercase FOR ALL OPTIONS ONLY
+//document.write('<DIV id="menulocation">');
+//document.write('<TABLE cellpadding="0" cellspacing="0" border="0" width="400"><tr><td class="menutrans">');
+
+document.write('<TABLE cellpadding="0" cellspacing="0" border="0" width="100%" <tr><td width="200">');
+
+// START LINKS //
+document.write('<h3><a href="index.html" class="menu">HOME</a></h3>');
+document.write('<h3><a class="menu">SUBJECT </a></h3>');
+document.write('<h3><a href="cinematica.html" class="menu">CINEMATICA</a></h3>');
+document.write('<ul>');
+document.write('<li><a href="index_blank.html">Teoria</a></li>');
+document.write('<li><a href="cinematica_slides_da_submenu.html">Presentazioni</a></li>');
+document.write('<li><a href="cinematica_esercizi_da_submenu.html">Esercizi</a></li>');
+document.write('<li><a href="index_blank.html">Lab</a></li>');
+document.write('<li><a href="cinematica_simulazioni_da_submenu.html">Simulazioni</a></li>');
+document.write('<li><a href="index_blank.html">Link</a></li>');
+document.write('</ul>');
+document.write('<br>');
+document.write('<h3><a href="index_blank.html" class="menu">DINAMICA</a></h3>');
+
+document.write('<ul>');
+document.write('<li><a href="index_blank.html">Teoria</a></li>');
+document.write('<li><a href="index_blank.html">Presentazioni</a></li>');
+document.write('<li><a href="index_blank.html">Esercizi</a></li>');
+document.write('<li><a href="index_blank.html">Lab</a></li>');
+document.write('<li><a href="index_blank.html">Simulazioni</a></li>');
+document.write('<li><a href="index_blank.html">Link</a></li>');
+document.write('</ul>');
+document.write('<br>');
+
+document.write('</td></tr></table></DIV>');
+
+//document.write('</td></tr></table></DIV>');
+//  End -->
---
+++ menu_prova.js
@@ -0,0 +1,72 @@
+// JavaScript Document
+
+<!-- Begin
+
+// NOTE: If you use a ' add a slash before it like this \'
+// USE lowercase FOR ALL OPTIONS ONLY
+//document.write('<DIV id="menulocation">');
+//document.write('<TABLE cellpadding="0" cellspacing="0" border="0" width="400"><tr><td class="menutrans">');
+
+                <nav>
+                    <a href="index_prova.html">Home</a>
+                    <a href="chi-siamo.html">Chi siamo</a>
+                    <a href="crediti.html">Crediti</a> |
+                    <div class="menu-container">
+                        <span class="menu-icon">☰</span>
+                        <div class="menu-box">
+    						<a href="index.html"><strong>HOME</strong></a>
+							<a href="cinematica.html"><strong>CINEMATICA</strong></a>
+    						<a href="moto_uniforme.html">Moto rettilineo uniforme</a>
+    						<a href="moto_accelerato.html">Moto uniformemente accelerato</a>							
+							
+							<a href="dinamica.html"><strong>DINAMICA</strong></a>						
+    						<a href="1-principio.html">1° Principio</a>
+    						<a href="2-principio.html">2° Principio</a>
+    						<a href="3-principio.html">3° Principio</a>
+    						<a href="Forze.html">Forze conservative e dissipative</a>
+    						<a href="lavoro.html">Lavoro</a>
+    						<a href="cinetica.html">Energia cinetica</a>
+    						<a href="potenziale.html">Energia potenziale</a>
+    						<a href="meccanica.html">Energia meccanica</a>							
+							
+							<a href="ottica.html"><strong>OTTICA</strong></a>
+    						<a href="leggi_ottica.html">Leggi Ottica</a>
+    						<a href="specchi.html">Gli Specchi</a>							
+    						<a href="lenti.html">Le Lenti</a>
+							
+    						<a href="chicco.html"><strong>Storie di Chicco</strong></a>
+						</div>
+
+                    </div>
+                </nav>
+
+
+<div class="menu-container">
+document.write('<h3><a href="index_prova.html" class="menu-box">HOME</a></h3>');
+document.write('<h3><a class="menu-box">SUBJECT </a></h3>');
+document.write('<h3><a href="cinematica.html" class="menu-box">CINEMATICA</a></h3>');
+document.write('<ul>');
+document.write('<li><a href="index_blank.html">Teoria</a></li>');
+document.write('<li><a href="cinematica_slides_da_submenu.html">Presentazioni</a></li>');
+document.write('<li><a href="cinematica_esercizi_da_submenu.html">Esercizi</a></li>');
+document.write('<li><a href="index_blank.html">Lab</a></li>');
+document.write('<li><a href="cinematica_simulazioni_da_submenu.html">Simulazioni</a></li>');
+document.write('<li><a href="index_blank.html">Link</a></li>');
+document.write('</ul>');
+document.write('<br>');
+document.write('<h3><a href="index_blank.html" class="menu">DINAMICA</a></h3>');
+
+document.write('<ul>');
+document.write('<li><a href="index_blank.html">Teoria</a></li>');
+document.write('<li><a href="index_blank.html">Presentazioni</a></li>');
+document.write('<li><a href="index_blank.html">Esercizi</a></li>');
+document.write('<li><a href="index_blank.html">Lab</a></li>');
+document.write('<li><a href="index_blank.html">Simulazioni</a></li>');
+document.write('<li><a href="index_blank.html">Link</a></li>');
+document.write('</ul>');
+document.write('<br>');
+</div>
+
+
+//document.write('</td></tr></table></DIV>');
+//  End -->
